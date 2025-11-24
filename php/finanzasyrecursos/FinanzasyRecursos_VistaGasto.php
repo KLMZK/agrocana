a46bf78fef9e3d2e1df2dd71dc2de765a69a7b90
CapibARA28-Mx <cadenasanchezarantxa@gmail.com>
2025-11-23 18:01:12 -0600

    Select e Insert de Finanzasy Recursos Terminado

--- php/finanzasyrecursos/FinanzasyRecursos_VistaGasto.php
+++ php/finanzasyrecursos/FinanzasyRecursos_VistaGasto.php
@@ -2,14 +2,25 @@
 include "../../conexion/conexion.php";
 header('Content-Type: application/json');
 
-$gA = $conexion->query("SELECT SUM(COSTO) AS total FROM actividades WHERE MONTH(FECHA) = MONTH(CURDATE())AND YEAR(FECHA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
-$gM = $conexion->query("SELECT SUM(COSTO) AS total FROM mantenimientos WHERE MONTH(FECHA) = MONTH(CURDATE())AND YEAR(FECHA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
-$gE = $conexion->query("SELECT SUM(COSTO) AS total FROM envios WHERE MONTH(FECHASALIDA) = MONTH(CURDATE())AND YEAR(FECHASALIDA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
-$gI = $conexion->query("SELECT SUM(CANTIDAD * PRECIOU) AS total FROM items")->fetch_assoc()['total'] ?? 0;//CHECAR
+$gActividades = $conexion->query("SELECT SUM(COSTO) AS total FROM actividades WHERE MONTH(FECHA) = MONTH(CURDATE())AND YEAR(FECHA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
+$gMantenimientos = $conexion->query("SELECT SUM(COSTO) AS total FROM mantenimientos WHERE MONTH(FECHA) = MONTH(CURDATE())AND YEAR(FECHA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
+$gEnvios = $conexion->query("SELECT SUM(COSTO) AS total FROM envios WHERE MONTH(FECHASALIDA) = MONTH(CURDATE())AND YEAR(FECHASALIDA) = YEAR(CURDATE())")->fetch_assoc()['total'] ?? 0;
+$gInsumos = $conexion->query(
+"SELECT SUM(encarga.CANTIDAD * items.PRECIOU) 
+AS total FROM items
+LEFT JOIN encarga ON encarga.CVE_ITEM = items.CVE_ITEM
+LEFT JOIN pedidos ON pedidos.CVE_PEDIDO = encarga.CVE_PEDIDO
+WHERE MONTH(pedidos.FECHAENTREGA) = MONTH(CURDATE())
+AND YEAR(pedidos.FECHAENTREGA)  = YEAR(CURDATE()) 
+AND pedidos.INGRESO=0")->fetch_assoc()['total'] ?? 0;
 
-$totalGasto = $gA + $gM + $gE + $gI;
+$totalGasto = $gActividades + $gMantenimientos + $gEnvios + $gInsumos;
 
 echo json_encode([
+    "gActividades" => $gActividades,
+    "gMantenimientos" => $gMantenimientos,
+    "gEnvios" => $gEnvios,
+    "gInsumos" => $gInsumos,
     "totalGastos" => $totalGasto
 ]);
 ?>
